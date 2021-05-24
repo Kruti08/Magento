ce1907e63cc6ce1d19a0550c875b59fd47866b13
Kruti Panchal <kbpanchal34@gmail.com>
2021-05-24 20:18:27 +0530

    Delete app directory

--- app/code/local/Ccc/P1/Block/P1.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Ccc_P1_Block_P1 extends Mage_Core_Block_Template
-{
-
-}
--- app/code/local/Ccc/P1/Model/Resource/P1/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Ccc_P1_Model_Resource_P1_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
-{
-
-    public function _construct()
-    {
-        $this->_init('ccc_p1/p1');
-    }
-}
